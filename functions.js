b3b149081fcea7d107949b88551e8069ba1ddd45
aleks-and-r <aleksandar.kosanovic88@gmail.com>
2021-01-07 16:23:56 +0100

    Push state implemented

---
+++ functions.js
@@ -0,0 +1,188 @@
+//residential
+function ovca() {
+  history.pushState(null, null, '/projects/01-residential/ovca.html');
+  console.log('changing');
+  location.reload()
+}
+function rasadnik() {
+  history.pushState(null, null, '/projects/01-residential/rasadnik.html');
+  console.log('changing');
+  location.reload()
+}
+function kamendin() {
+  history.pushState(null, null, '/projects/01-residential/kamendin.html');
+  console.log('changing');
+  location.reload()
+}
+function ribnjak() {
+  history.pushState(null, null, '/projects/01-residential/ribnjak.html');
+  console.log('changing');
+  location.reload()
+}
+function niksic() {
+  history.pushState(null, null, '/projects/01-residential/niksic.html');
+  console.log('changing');
+  location.reload()
+}
+function topolska() {
+  history.pushState(null, null, '/projects/01-residential/topolska.html');
+  console.log('changing');
+  location.reload()
+}
+//commercial
+function carine() {
+  history.pushState(null, 'commercial', '/projects/02-commercial/carine.html');
+  console.log('changing');
+  location.reload()
+}
+function ulcinj() {
+  history.pushState(null, 'commercial', '/projects/02-commercial/ulcinj.html');
+  console.log('changing');
+  location.reload()
+}
+function igalo() {
+  history.pushState(null, 'commercial', '/projects/02-commercial/igalo.html');
+  console.log('changing');
+  location.reload()
+}
+function pariski_magazin() {
+  history.pushState(null, 'commercial', '/projects/02-commercial/pariski_magazin.html');
+  console.log('changing');
+  location.reload()
+}
+function nork() {
+  history.pushState(null, 'commercial', '/projects/02-commercial/nork.html');
+  console.log('changing');
+  location.reload()
+}
+function riblja() {
+  history.pushState(null, 'commercial', '/projects/02-commercial/riblja.html');
+  console.log('changing');
+  location.reload()
+}
+//administrative
+function conference() {
+  history.pushState(null, 'commercial', '/projects/03-administrative/bg_conference.html');
+  console.log('changing');
+  location.reload()
+}
+function minakva() {
+  history.pushState(null, null, '/projects/03-administrative/minaqua.html');
+  console.log('changing');
+  location.reload()
+}
+function municipality() {
+  history.pushState(null, null, '/projects/03-administrative/radnicka.html');
+  console.log('changing');
+  location.reload()
+}
+function academy() {
+  history.pushState(null, null, '/projects/03-administrative/canu.html');
+  console.log('changing');
+  location.reload()
+}
+function boulevard() {
+  history.pushState(null, null, '/projects/03-administrative/matijevic.html');
+  console.log('changing');
+  location.reload()
+}
+function pavilion() {
+  history.pushState(null, null, '/projects/03-administrative/it_centar.html');
+  console.log('changing');
+  location.reload()
+}
+
+// culture 
+function sipcanik() {
+  history.pushState(null, null, '/projects/04-culture/sipcanik.html');
+  console.log('changing');
+  location.reload()
+}
+function library() {
+  history.pushState(null, null, '/projects/04-culture/ns_library.html');
+  console.log('changing');
+  location.reload()
+}
+function ugm() {
+  history.pushState(null, null, '/projects/04-culture/ugm.html');
+  console.log('changing');
+  location.reload()
+}
+function hala() {
+  history.pushState(null, null, '/projects/04-culture/beton_hala.html');
+  console.log('changing');
+  location.reload()
+}
+function museum() {
+  history.pushState(null, null, '/projects/04-culture/museum.html');
+  console.log('changing');
+  location.reload()
+}
+function kolo() {
+  history.pushState(null, null, '/projects/04-culture/kolo.html');
+  console.log('changing');
+  location.reload()
+}
+
+//public space
+function savskitrg() {
+  history.pushState(null, null, '/projects/05-public_space/savski_trg.html');
+  console.log('changing');
+  location.reload()
+}
+function cer() {
+  history.pushState(null, null, '/projects/05-public_space/cer.html');
+  console.log('changing');
+  location.reload()
+}
+function dadov() {
+  history.pushState(null, null, '/projects/05-public_space/dadov.html');
+  console.log('changing');
+  location.reload()
+}
+function railway() {
+  history.pushState(null, null, '/projects/05-public_space/railway_station.html');
+  console.log('changing');
+  location.reload()
+}
+function obeda() {
+  history.pushState(null, null, '/projects/05-public_space/obeda.html');
+  console.log('changing');
+  location.reload()
+}
+function bus() {
+  history.pushState(null, null, '/projects/05-public_space/bus_station.html');
+  console.log('changing');
+  location.reload()
+}
+//interior
+function jevremova() {
+  history.pushState(null, null, '/projects/06-interior/jevremova.html');
+  console.log('changing');
+  location.reload()
+}
+function spa_centre() {
+  history.pushState(null, null, '/projects/06-interior/spa_centre.html');
+  console.log('changing');
+  location.reload()
+}
+function addiko() {
+  history.pushState(null, null, '/projects/06-interior/addiko.html');
+  console.log('changing');
+  location.reload()
+}
+function tent_a() {
+  history.pushState(null, null, '/projects/06-interior/tent_a.html');
+  console.log('changing');
+  location.reload()
+}
+function iq_computers() {
+  history.pushState(null, null, '/projects/06-interior/iq_computers.html');
+  console.log('changing');
+  location.reload()
+}
+function forest_moscow() {
+  history.pushState(null, null, '/projects/06-interior/forest_moscow.html');
+  console.log('changing');
+  location.reload()
+}
